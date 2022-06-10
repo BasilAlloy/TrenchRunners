f90819e2b0b9c00675c889bc299e6c8b3098cddd
BasilAlloy <101365921+BasilAlloy@users.noreply.github.com>
2022-06-09 21:01:02 -0400

    Add moody lighting
    The ship now has headlights and the trench is dark.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,14 +12,18 @@ public class Movement : MonoBehaviour
 	[SerializeField] private float initialThrust = 25f;	
 	// How much the ship speeds up each tick
 	[SerializeField] private float acceleration = 0.005f;
+	// How fast the ship can roll
+	[SerializeField] private float RollSpeed = 64;
 	// Direction the ship is moving in
 	private Vector3 direction = Vector3.zero;
 	// Records the start position of the ship so we can reset to it
 	private Vector3 startPosition;
+
+	private float rollSpeed;
 	
 	private void Start() {
         startPosition = gameObject.transform.position;
-
+		rollSpeed = 2 * Mathf.PI/360 * RollSpeed;
 		// Initialize Ship state
 		resetShip();
     }
@@ -31,13 +35,24 @@ public class Movement : MonoBehaviour
 		float vertical = Input.GetAxisRaw("Vertical");
 		direction = new Vector3(horizontal, vertical, 0f).normalized;
 
+		// Move ship towards the mouse when lmb is held down
+		// if (Input.GetMouseButton(0))
+		// {
+		// 	direction = new Vector3(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2, 0f).normalized;
+		// }
+
+		// Rotate the ship left if lmb is held down, rotate right if rmb is held down.
+		var rotateDir = 0;
 		if (Input.GetMouseButton(0))
-		{
-			direction = new Vector3(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2, 0f).normalized;
+		{	rotateDir += 1;
+		} 
+		if (Input.GetMouseButton(1))
+		{	rotateDir -= 1;
 		}
 
 		Vector3 moveVector = transform.TransformDirection(direction) * speed;
 		rb.velocity = new Vector3(moveVector.x, moveVector.y, rb.velocity.z + acceleration);
+		rb.angularVelocity = new Vector3(0,0, rotateDir * rollSpeed);
 	}
 
 	// Whenever the ship collides with an object tagged with killship, reset the ship.
