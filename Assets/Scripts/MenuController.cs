f6ae486083f1a8ef65a61f67ce5525272510c8cc
Mjb <mjb117@terpmail.umd.edu>
2022-06-10 23:06:20 -0400

    Add audio settings
    Added a menu for controlling volume.  Stores the audio setting in PlayerPrefs and loads the value on game start.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+	[Header("Volume Setting")]
+	[SerializeField] private TextMeshProUGUI volumeTextValue = null;
+	[SerializeField] private Slider volumeSlider = null;
+	[SerializeField] private float defaultVolume = 0.7f;
+
+	[Header("Confirmation")]
+	[SerializeField] private GameObject confirmationPrompt = null;
+
+	[Header("Level to Load")]
     public string _newGameLevel;
 
-    public void NewGame()
+	// Start is called before the first frame update
+	void Start()
+	{
+		float volume = PlayerPrefs.GetFloat("masterVolume", defaultVolume);
+		AudioListener.volume = volume;
+		volumeSlider.value = volume;
+		volumeTextValue.text = volume.ToString("P0");
+	}
+
+	public void NewGame()
 	{
 		SceneManager.LoadScene(_newGameLevel);
 	}
@@ -16,5 +35,32 @@ public class MenuController : MonoBehaviour
 	public void Exit()
 	{
 		Application.Quit();
+	}
+
+	public void SetVolume(float volume)
+	{
+		AudioListener.volume = volume;
+		volumeTextValue.text = volume.ToString("P0");
+	}
+
+	public void VolumeApply()
+	{
+		PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+		StartCoroutine(ConfirmationBox());
+	}
+
+	public void Reset()
+	{
+		AudioListener.volume = defaultVolume;
+		volumeSlider.value = defaultVolume;
+		volumeTextValue.text = defaultVolume.ToString("P0");
+		VolumeApply();
+	}
+
+	public IEnumerator ConfirmationBox()
+	{
+		confirmationPrompt.SetActive(true);
+		yield return new WaitForSeconds(2);
+		confirmationPrompt.SetActive(false);
 	}
 }
